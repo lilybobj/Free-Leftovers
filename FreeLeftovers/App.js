8c761eaaafcb5020d7c304f028bf99702de89432
pegifenu <110061199+pegifenu@users.noreply.github.com>
2024-03-03 12:06:18 -0800

    added tab screens

--- FreeLeftovers/App.js
+++ FreeLeftovers/App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './screens/HomeScreen';
+import PointsScreen from './screens/PointsScreen';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='HomeScreen' component={HomeScreen} />
+        <Tab.Screen name='PointsScreen' component={PointsScreen} />
+      </Tab.Navigator> 
+    </NavigationContainer>
+    /*
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="hi"></Button>
     </View>
+    */
   );
 }
 
