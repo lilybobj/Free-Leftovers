b433f7c800c1c18fdd67b40b48f6932e702d1b10
lilybobj <156983048+lilybobj@users.noreply.github.com>
2024-03-03 10:15:09 -0800

    Add files via upload

---
+++ Free_LeftoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  Free_LeftoversApp.swift
+//  Free Leftovers
+//
+//  Created by Lily Jiang on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Free_LeftoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
