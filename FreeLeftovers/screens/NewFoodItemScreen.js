815669b1255ef4f57298ad422daaba5c2a013b15
lilybobj <lilybobj@gmail.com>
2024-03-03 14:06:52 -0800

    sf

--- FreeLeftovers/screens/NewFoodItemScreen.js
+++ FreeLeftovers/screens/NewFoodItemScreen.js
@@ -3,21 +3,25 @@ import { View, Text, TextInput, Button } from 'react-native';
 import React, { useState } from 'react';
 
 
-const NewFoodItemScreen = ({ addFoodItem }) => {
-    const [foodName, setFoodName] = useState('');
+
+const NewFoodItemScreen = ({ navigation, route }) => {
+    const [newItemText, setNewItemText] = useState('');
 
     const handleAddFoodItem = () => {
-        addFoodItem(foodName);
-        setFoodName('');
-    }
+        if (newItemText.trim() !== '') {
+            route.params.onAddFoodItem(newItemText.trim());
+            navigation.goBack();
+        }
+    };
+    
 
     return (
         <View>
             <Text>Add New Food Item</Text>
             <TextInput
-                placeholder="Enter food name"
-                value={foodName}
-                onChangeText={setFoodName}
+                placeholder="Enter new item"
+                value={newItemText}
+                onChangeText={text => setNewItemText(text)}
             />
             <Button
                 title="Add"
